79689837bddee98610da8fc0f5a1839a57273932
rajananusha <ms.anusharajan@gmail.com>
2017-12-02 14:59:31 -0700

    Set up DB and db connection

--- server/Rest.py
+++ server/Rest.py
@@ -1,25 +1,32 @@
 from bottle import route, run, request
-from math import radians, cos, sin, asin, sqrt
-from collections import Counter
+# from math import radians, cos, sin, asin, sqrt
+# from collections import Counter
 import bottle
 import json
 import time
-import datetime
-import operator
-from dateutil import parser
+# import datetime
+# import operator
+# from dateutil import parser
 
 import pymysql
 conn = None
 
+# CREATE TABLE app_user (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, username CHAR(25), auth_token CHAR(100), points INT(9), streak INT(9), group_id INT(5), active BIT);
+# CREATE TABLE bets (group_id INT(5), bet_by CHAR(25), bet_on CHAR(25), bet INT(9));
+
 try:
-    conn = pymysql.connect(db='smartmeetingdb', user='group30', host='smartmeeting.cyrypi4sn74l.us-west-2.rds.amazonaws.com', passwd='12345678')
+    conn = pymysql.connect(db='gitclash', user='root', host='10.2.66.49', passwd='password')
 except:
     print "I am unable to connect to the database"
+
+@route('/hello', method='GET')
+def get_users():
+    return json.dumps({"message": "hello-world"})
 
 @route('/users', method='GET')
 def get_users():
     with conn.cursor(pymysql.cursors.DictCursor) as cur:
-        get_all = "SELECT * from `user`"
+        get_all = "SELECT * from `app_user`"
         cur.execute(get_all)
         data = cur.fetchall()
         conn.commit()
@@ -28,11 +35,10 @@ def get_users():
 @route('/user', method='POST')
 def create_user():
     with conn.cursor() as cur:
-        insert = "INSERT into `user` (`username`, `password`, `name`, `email`, `phone`) values (%s, %s, %s, %s, %s)"
-        username = request.forms.get('username')
-        password = request.forms.get('password')
-        name = request.forms.get('name')
-        email = request.forms.get('email')
-        phone = request.forms.get('phone')
-        cur.execute(insert, (username, password, name, email, phone))
+        insert = "INSERT into `app_user` (`username`, `auth_token`, `points`, `streak`, `group_id`, `active`) values (%s, %s, %s, %s, %s, %s)"
+        username = request.forms.get('uname')
+        # password = request.forms.get('auth_token')
+        cur.execute(insert, (username, 'dummy_auth_token', 0, 0, 1, 1))
         conn.commit()
+
+
