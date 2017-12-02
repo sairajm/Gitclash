1bcc6945d8b3ba682626d87cfae0f8f5c875f5a9
Rangaraj <Rangaraj.sundar@Rangarajs-MacBook-Pro.local>
2017-12-02 14:11:43 -0700

    Adding components

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,6 +17,18 @@ class App extends Component {
       </div>
     );
   }
+}
+
+function getTabs() {
+  const tabsInstance = (
+    <Tabs defaultActiveKey={1} animation={false} id="noanim-tab-example">
+      <Tab eventKey={1} title="Tab 1">Tab 1 content</Tab>
+      <Tab eventKey={2} title="Tab 2">Tab 2 content</Tab>
+      <Tab eventKey={3} title="Tab 3" disabled>Tab 3 content</Tab>
+    </Tabs>
+  );
+
+  ReactDOM.render(tabsInstance, mountNode);
 }
 
 export default App;
